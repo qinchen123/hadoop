9c8dff9f10d7cbe82ff8afd413f48d1c220fe498
qinchen <qinchen@microsoft.com>
2018-06-16 18:47:02 +0800

    message

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestCapacityScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestCapacityScheduler.java
@@ -2022,10 +2022,10 @@ public class TestCapacityScheduler {
     application_0.addNodeManager(host_0, 1234, nm_0);
     application_0.addNodeManager(host_1, 1234, nm_1);
 
-    Resource capability_0_0 = Resources.createResource(3 * GB, 1, 1);
+    Resource capability_0_0 = Resources.createResource(3 * GB, 1);
     application_0.addResourceRequestSpec(priority_1, capability_0_0);
 
-    Resource capability_0_1 = Resources.createResource(2 * GB, 1, 1);
+    Resource capability_0_1 = Resources.createResource(2 * GB, 1);
     application_0.addResourceRequestSpec(priority_0, capability_0_1);
 
     Task task_0_0 =
@@ -2137,10 +2137,10 @@ public class TestCapacityScheduler {
 
     application_0.addNodeManager(host_0, 1234, nm_0);
 
-    Resource capability_0_0 = Resources.createResource(3 * GB, 1, 1);
+    Resource capability_0_0 = Resources.createResource(3 * GB, 1);
     application_0.addResourceRequestSpec(priority_1, capability_0_0);
 
-    Resource capability_0_1 = Resources.createResource(2 * GB, 1, 1);
+    Resource capability_0_1 = Resources.createResource(2 * GB, 1);
     application_0.addResourceRequestSpec(priority_0, capability_0_1);
 
     Task task_0_0 =
@@ -2192,10 +2192,10 @@ public class TestCapacityScheduler {
     application_0.addNodeManager(host_0, 1234, nm_0);
     application_0.addNodeManager(host_1, 1234, nm_1);
 
-    Resource capability_0_0 = Resources.createResource(3 * GB, 1, 1);
+    Resource capability_0_0 = Resources.createResource(3 * GB, 1);
     application_0.addResourceRequestSpec(priority_1, capability_0_0);
 
-    Resource capability_0_1 = Resources.createResource(2 * GB, 1, 1);
+    Resource capability_0_1 = Resources.createResource(2 * GB, 1);
     application_0.addResourceRequestSpec(priority_0, capability_0_1);
 
     Task task_0_0 =
